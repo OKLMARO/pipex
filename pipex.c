d97093871698470cd2e712aaf6a9091b992c2616
Omar Amairi <oamairi@f1br8s5.clusters.42paris.fr>
2025-07-24 13:25:45 +0200

    presque fini

--- pipex.c
+++ pipex.c
@@ -6,24 +6,11 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 13:21:41 by oamairi           #+#    #+#             */
-/*   Updated: 2025/07/22 18:11:13 by oamairi          ###   ########.fr       */
+/*   Updated: 2025/07/24 13:24:37 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-void	free_double(char **tab_str)
-{
-	int	i;
-
-	i = 0;
-	while (tab_str[i])
-	{
-		free(tab_str[i]);
-		i++;
-	}
-	free(tab_str);
-}
 
 char	**get_path(char **env)
 {
@@ -67,52 +54,59 @@ char	*valid_command(char *cmd, char **path)
 	return (NULL);
 }
 
-void	first(char *file_in, int pip[2], char *all_cmd, char **cmd, char **env)
+void	first(char *file_in, int pip[2], char *all_cmd, char **cmd)
 {
 	pid_t	fils;
 	int		file;
 
 	file = open(file_in, O_RDONLY);
 	if (!file)
-		return (free(all_cmd), free_double(cmd), perror("Fichier de lecture manquant"));
+	{
+		free(all_cmd);
+		free_double(cmd);
+		return (perror("Fichier de lecture manquant"), exit(2));
+	}
 	fils = fork();
 	if (fils == 0)
 	{
-		if(dup2(file, 0) == -1)
-			return (free(all_cmd), free_double(cmd), perror("Erreur dans le dup2"), exit(2));
-		if(dup2(pip[1], 1) == -1)
-			return (free(all_cmd), free_double(cmd), perror("Erreur dans le dup2"), exit(2));
-		execve(all_cmd, cmd, env);
+		if (dup2(file, 0) == -1 || dup2(pip[1], 1) == -1)
+		{
+			free(all_cmd);
+			free_double(cmd);
+			return (perror("Erreur dans le dup2"), exit(2));
+		}
+		execve(all_cmd, cmd, NULL);
 	}
 	waitpid(fils, NULL, 0);
-	close(file);
-	close(pip[1]);
-	free(all_cmd);
-	free_double(cmd);
+	return (close(file), close(pip[1]), free(all_cmd), free_double(cmd));
 }
 
-void	second(char *file_out, int pip[2], char *all_cmd, char **cmd, char **env)
+void	second(char *file_out, int pip[2], char *all_cmd, char **cmd)
 {
 	pid_t	fils;
 	int		file;
 
 	file = open(file_out, O_WRONLY | O_TRUNC | O_CREAT, 0777);
 	if (!file)
-		return (free(all_cmd), free_double(cmd), perror("Probleme lors de l'ouverture du fichier de sortie"), exit(2));
+	{
+		free(all_cmd);
+		free_double(cmd);
+		perror("Probleme lors de l'ouverture du fichier de sortie");
+		return (exit(2));
+	}
 	fils = fork();
 	if (fils == 0)
 	{
-		if(dup2(file, 1) == -1)
-			return (free(all_cmd), free_double(cmd), perror("Erreur dans le dup2"), exit(2));
-		if(dup2(pip[0], 0) == -1)
-			return (free(all_cmd), free_double(cmd), perror("Erreur dans le dup2"), exit(2));
-		execve(all_cmd, cmd, env);
+		if (dup2(file, 1) == -1 || dup2(pip[0], 0) == -1)
+		{
+			free(all_cmd);
+			free_double(cmd);
+			return (perror("Erreur dans le dup2"), exit(2));
+		}
+		execve(all_cmd, cmd, NULL);
 	}
 	waitpid(fils, NULL, 0);
-	close(file);
-	close(pip[0]);
-	free(all_cmd);
-	free_double(cmd);
+	return (close(file), close(pip[0]), free(all_cmd), free_double(cmd));
 }
 
 int	main(int argc, char **argv, char **env)
@@ -123,7 +117,7 @@ int	main(int argc, char **argv, char **env)
 	char	**path;
 
 	if (argc != 5)
-		return	(perror("Incorrect number of arguments !"), exit(2), 0);
+		return (perror("Incorrect number of arguments !"), exit(2), 0);
 	path = get_path(env);
 	pipe(pip);
 	cmd = ft_split(argv[2], ' ');
@@ -131,13 +125,15 @@ int	main(int argc, char **argv, char **env)
 		return (perror("Malloc crash"), exit(2), 0);
 	all_cmd = valid_command(cmd[0], path);
 	if (!all_cmd)
-		return (free_double(cmd), perror("Commande introuvable ou non executable"), exit(2), 0);
-	first(argv[1], pip, all_cmd, cmd, env);
+		return (free_double(cmd),
+			perror("Commande introuvable ou non executable"), exit(2), 0);
+	first(argv[1], pip, all_cmd, cmd);
 	cmd = ft_split(argv[3], ' ');
 	if (!cmd)
 		return (perror("Malloc crash"), 1);
 	all_cmd = valid_command(cmd[0], path);
 	if (!all_cmd)
-		return (free_double(cmd), perror("Commande introuvable ou non executable"), exit(2), 0);
-	return (second(argv[4], pip, all_cmd, cmd, env), 0);
+		return (free_double(cmd),
+			perror("Commande introuvable ou non executable"), exit(2), 0);
+	return (second(argv[4], pip, all_cmd, cmd), 0);
 }
